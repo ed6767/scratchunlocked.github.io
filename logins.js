f33feb33e5f17a797e2324ab81eb14c94bb913f5
EDXT <ed@edxtech.uk>
2017-06-11 18:39:09 +0100

    Create logins.js

--- logins.js
+++ logins.js
@@ -2,5 +2,7 @@
 document.title = "scratchUnlocked"
   var content = '<iframe src="https://edxtech.github.io/scratchunlocked.github.io/logingin.htm" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>';
     document.getElementsByTagName('body')[0].innerHTML = content; 
-alert($.getJSON("https://scratch.mit.edu/session/"));
+    $.ajax({url: "demo_test.txt", success: function(result){
+        alert(result);
+    }});
 alert("Up and running");
